6d91a9b9c4d1ddaf8ebad2de46ee007f9a0d015d
AnnaKulikovskikh <59337789+AnnaKulikovskikh@users.noreply.github.com>
2022-06-27 21:13:10 +0300

    Create TikTak.js

---
+++ src/components/TikTak.js
@@ -0,0 +1,21 @@
+export default function TikTak(props) {
+
+    const degMin = 6  //360 градусов / 60 минут или секунд
+    const h = props.time.slice(0,2) * 30
+    const m = props.time.slice(3,5) * degMin
+    const s = props.time.slice(6,8) * degMin
+
+    return (
+        <div className="analog-clock">
+            <div className="hour">
+                <div class="hr" id="hr" style={{transform: `rotateZ(${(h) + (m/12)}deg)` }}></div>
+            </div>
+            <div className="min">
+                <div class="mn" id="mn"  style={{transform: `rotateZ(${m}deg)` }}></div>
+            </div>
+            <div className="sec">
+                <div class="sc" id="sc" style={{transform: `rotateZ(${s}deg)` }}></div>
+            </div>
+        </div>
+    )
+}
